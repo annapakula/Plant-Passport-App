18f1124f1b46e2b7d08fdbbe29765857e851ddf2
Anna Mazurek <anna.mazurek.93@gmail.com>
2020-06-06 12:26:28 +0200

    Updates

---
+++ precache-manifest.1240b556a8eb19b6219c42de362f683e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ba57f44a4eecfbe36efa96494f4e83",
+    "url": "/Plant-Passport-App/index.html"
+  },
+  {
+    "revision": "349a54629f084999a93b",
+    "url": "/Plant-Passport-App/static/css/main.fd8b55b8.chunk.css"
+  },
+  {
+    "revision": "c4ef205fc0b7a2fe2d22",
+    "url": "/Plant-Passport-App/static/js/2.b4cbcbe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Plant-Passport-App/static/js/2.b4cbcbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349a54629f084999a93b",
+    "url": "/Plant-Passport-App/static/js/main.73c274f8.chunk.js"
+  },
+  {
+    "revision": "fd0ea2737e2b5a2fe65e",
+    "url": "/Plant-Passport-App/static/js/runtime-main.18092e10.js"
+  },
+  {
+    "revision": "7b4534d284b8ca4889f4c4d8b56d1f9c",
+    "url": "/Plant-Passport-App/static/media/flag-of-europe.7b4534d2.svg"
+  }
+]);
